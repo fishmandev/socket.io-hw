4735efe5c5984a3a7ac26ea6cae5fd856775f3f6
Dmitriy Fishman <fishman.code@gmail.com>
2015-08-15 20:35:06 +0300

    Simple chat implementation

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ var users = require('./routes/users');
 
 var app = express();
 
+var socketIo = require('socket.io');
+var io = socketIo();
+app.io = io;
+
+io.on('connection', function(socket){
+  console.log('a user connected', socket.id);
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected', socket.id);
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
